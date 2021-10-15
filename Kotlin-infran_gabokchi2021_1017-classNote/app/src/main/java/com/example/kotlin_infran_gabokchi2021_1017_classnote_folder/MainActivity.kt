489267809fbb9561e1d0f6d2f3c089918a04ce81
IG KIM-dalcom <igdev@dalcom.com>
2021-10-15 11:37:28 -0400

    90. Toast.makeText , android:id="@+id/bts_image_1"

--- Kotlin-infran_gabokchi2021_1017-classNote/app/src/main/java/com/example/kotlin_infran_gabokchi2021_1017_classnote_folder/MainActivity.kt
+++ Kotlin-infran_gabokchi2021_1017-classNote/app/src/main/java/com/example/kotlin_infran_gabokchi2021_1017_classnote_folder/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.kotlin_infran_gabokchi2021_1017_classnote_folder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        c90. findViewbyId, toast
+//        1.val ~~ = findViewById<~~>(R.id.~~~)
+         val image1 = findViewById<ImageView>(R.id.bts_image_1)
+
+        image1.setOnClickListener {
+//            Toast.makeText(, "", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(, "gge", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, text:"number 1 click" , Toast.LENGTH_SHORT).show()
+        }
+//        2
     }
 }
